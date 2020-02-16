1a202a6a7e3de0860a0635cc36bb1004f6d4ff5a
Ming l <61004171+ming9080@users.noreply.github.com>
2020-02-16 08:56:10 +0200

    Delete run_docker_scheduler.sh

--- run_docker_scheduler.sh
+++
@@ -1,3 +0,0 @@
-sudo nvidia-docker run -it --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  -v ~/dev/:/root/sharedfolder -v  ~/data/:/root/data nccl-torch-cuda-10.0 bash -c 'cd /root/sharedfolder/production/v-gpu/byteps/docker; ./scheduler_worker.sh; exec "${SHELL:-sh}"'
-
-#/root/sharedfolder/production/v-gpu/byteps/docker/scheduler_worker.sh
