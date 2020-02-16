fadee37c3bc5a00711136219878c976633aab612
Ming l <61004171+ming9080@users.noreply.github.com>
2020-02-16 08:56:18 +0200

    Delete run_docker_server.sh

--- run_docker_server.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -it --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  -v ~/dev/:/root/sharedfolder -v  ~/data/:/root/data nccl-torch-cuda-10.0 bash -c 'cd /root/sharedfolder/production/v-gpu/byteps/docker; ./server_worker.sh; exec "${SHELL:-sh}"'
