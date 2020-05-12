38b1f81fd962dffcfc1fc6bf3e240ce27cbc5925
Ming l <61004171+ming9080@users.noreply.github.com>
2020-05-12 13:17:55 +0300

    Add files via upload

---
+++ cuda-10/cuda-10.0/jupyter_notebook_config.py
@@ -0,0 +1,10 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 7842
+c.NotebookApp.open_browser = False
+c.MultiKernelManager.default_kernel_name = 'python3'
+
+# sets a password if PASSWORD is set in the environment
+c.NotebookApp.token = ''
+#c.NotebookApp.password = ''
+# password is 
+c.NotebookApp.password='sha1:65db47cf7e0d:d440485d58ec9fcc8b587c0aa96864f2f1816edd'
